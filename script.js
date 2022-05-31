28ae9590897de92856f3f45bd607dbed8780388d
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-05-31 15:20:52 -0700

    Added book tiles and footer.

---
+++ script.js
@@ -0,0 +1,10 @@
+const stirringPower = document.getElementById("sp-amazon");
+const acceptingPower = document.getElementById("ap-amazon");
+
+stirringPower.onclick = function(){
+    location.href = "https://www.amazon.com/Stirring-Power-Evolution-Magic-Book-ebook/dp/B01N8R12LY";
+};
+
+acceptingPower.onclick = function(){
+    location.href = "https://www.amazon.com/Accepting-Power-K-C-Blackbyrn-ebook/dp/B09QQHC2YP";
+}
