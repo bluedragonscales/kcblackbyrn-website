e8d08a9419bf053685d82e4165453e990891ac40
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-05-31 22:05:21 -0700

    Finished final website.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Buttons to go to specific book pages on Amazon to buy them.
 const stirringPower = document.getElementById("sp-amazon");
 const acceptingPower = document.getElementById("ap-amazon");
 
@@ -7,4 +8,18 @@ stirringPower.onclick = function(){
 
 acceptingPower.onclick = function(){
     location.href = "https://www.amazon.com/Accepting-Power-K-C-Blackbyrn-ebook/dp/B09QQHC2YP";
-}
+};
+
+
+
+// Buttons to go to social media pages for novels.
+const facebookButton = document.getElementById("facebook-button");
+const amazonButton = document.getElementById("amazon-button");
+
+facebookButton.onclick = function(){
+    location.href = "https://www.facebook.com/profile.php?id=100070407158426";
+};
+
+amazonButton.onclick = function(){
+    location.href = "https://www.amazon.com/~/e/B01MTY79EY";
+};
